9f87090a1b9a7f27e069acb08357729e6760d73a
Alex <alex.boulanger15@videotron.ca>
2022-06-06 17:36:47 -0400

    test pour config du vsc mapping

--- code/docker/postgres/init2.sql
+++ code/docker/postgres/init2.sql
@@ -2,3 +2,6 @@ CREATE SCHEMA extern_validation;
 
 CREATE VIEW extern_validation.validation AS
     SELECT * FROM extern_equipe.equipes;
+
+CREATE VIEW extern_validation.test AS
+SELECT * FROM extern_equipe.equipes;
