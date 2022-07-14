d9d3034238f9a80c482881562ee80ef32c8fc2c2
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2022-07-14 04:49:39 -0400

    Keycloak persistent et info utilisateur

---
+++ code/docker/keycloak/data/setdata.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#while true; do echo hello && sleep 5; done;
+
+timeout 300 /bin/bash -c 'until curl -sI -o /dev/null -w "%{http_code}\n" localhost:8080; do sleep 5; done;'
+
+echo "beginning of finalisation ...."
+/opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/ --realm master --user keycloak --password keycloak
+/opt/keycloak/bin/kcadm.sh create realms -s realm=Validation -s enabled=true -o
+/opt/keycloak/bin/kcadm.sh create clients -r Validation -f /var/tmp/frontend.json
+/opt/keycloak/bin/kcadm.sh create clients -r Validation -f /var/tmp/backend.json
+/opt/keycloak/bin/kcadm.sh create partialImport -r Validation -s ifResourceExists=OVERWRITE -f /var/tmp/users.json
+
+echo -e -n "\r"
+echo "server running ...."
+
+
+
+
--- code/src/main/resources/front-end/src/index.js
+++ code/src/main/resources/front-end/src/index.js
@@ -2,16 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Secured from "./Secured";
-import App from "./App";
-import {BrowserRouter} from "react-router-dom";
 
-const toRender = (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
-)
-
-
-ReactDOM.render(toRender, document.getElementById('root'));
+ReactDOM.render(<Secured/>, document.getElementById('root'));
 
 
